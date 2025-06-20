1499335cafbc77aa828dc35a87be19a656e4e77b
mohammed sinan <sinanshazz3107@gmail.com>
2025-06-20 11:37:08 +0530

    message

--- src/emailService.ts
+++ src/emailService.ts
@@ -1,8 +1,9 @@
 import emailjs from "emailjs-com";
 
-const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
-const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
-const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+
+const SERVICE_ID = "service_ixpwnky";
+const TEMPLATE_ID = "template_53tymdj";
+const PUBLIC_KEY = "XpwlYpgOgVOf3FuQd";
 
 
 export interface ContactFormData {
